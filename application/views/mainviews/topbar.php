04ee67249c6961565b473cff6b576b1f54038ce7
Xmal Morthen <askmeyou15@hotmail.com>
2012-03-05 18:38:28 -0600

    Avances

--- application/views/mainviews/topbar.php
+++ application/views/mainviews/topbar.php
@@ -6,7 +6,9 @@
 ?>
 
 <div class="bussinesheader">
-    <img class="logoempresa" src="<?php echo ResourceString("Etiquetas,SysInfo,Empresa,Logo"); ?>" alt="Logo Empresa" />
+    <div class="divlogo">
+        <img class="logoempresa" src="<?php echo ResourceString("Etiquetas,SysInfo,Empresa,Logo"); ?>" alt="Logo Empresa" />    
+    </div>    
     <div class="datainfo">
         <span class="nombreempresa"><?php echo ResourceString("Etiquetas,SysInfo,Empresa,NombreEmpresa"); ?></span>
         <span class="detalleempresa"><?php echo ResourceString("Etiquetas,SysInfo,Empresa,Detalle"); ?></span>
@@ -19,13 +21,10 @@
 </div>
 
 <div class="menuheader">                    
-    <nav style="border: 1px solid red; color: white;">
-        aqui va menu de opciones...!!!
-    </nav>
-    <form action="">
+    <form>
         <div class="inputsarea"> 
-            <input class="" type="text" placeholder="Username">
-            <input class="" type="password" placeholder="Password">
+            <input class="" type="text" placeholder="Nombre de usuario">
+            <input class="" type="password" placeholder="Contraseña">
         </div>
         <div class="btnarea"> 
             <button class="btnlogin" type="submit" title="Iniciar sesión"></button>
--- resources/css/topbar.php
+++ resources/css/topbar.php
@@ -2,17 +2,17 @@
 
 .topbar > .fill {background-color: #444; padding: 2px;}
 .bussinesheader{float: left;}
-.bussinesheader .logoempresa {position: absolute;width: 120px;height: 120px;}
-.bussinesheader .datainfo {display: block;margin: 2px 0 0 125px;}
+.bussinesheader .logoempresa {position: absolute;width: 140px;height: 140px;}
+.bussinesheader .datainfo {display: block;margin: 2px 0 0 145px;}
 .bussinesheader .nombreempresa{display: block;font-size: 2.5em;color: white;line-height: 1;}
 .bussinesheader .detalleempresa{font-size: 1.5em;color: #cacaca;}
-.bussinesheader .nombresistema{display: block;font-size: 2.3em;color:orange ;line-height: 1;line-height: 1.2;margin-top: 10px;}
+.bussinesheader .nombresistema{display: block;font-size: 2.3em;color:orange ;line-height: 1;line-height: 1.2;margin-top: 5px;}
 .bussinesheader .detallesistema{font-size: 1.2em;color:#cacaca;line-height: 1.2;float: left}
 .topbar hr {margin: 0 0;padding: 0 0;border: 0;width: 80%;color: #666;background-color: #666;height: 1px;float: left;}
-.menuheader{float: right;}
+.menuheader{float: right; margin-top: 40px; }
 .menuheader .inputsarea{float: left;margin: 0 3px;}
-.inputsarea input {display: block;width: 110px;}
+.inputsarea input {display: block; width: 140px; font-size: 1.1em;}
 .menuheader .btnarea{float: right;}
-.btnarea .btnlogin{height: 30px;width: 30px;background-image: url("<?php echo imgs_path(__IMAGESPATH_BTNS."/user.png"); ?>");margin: 20px 0 0 2px;}
+.btnarea .btnlogin{height: 30px;width: 30px;background-image: url("<?php echo imgs_path(__IMAGESPATH_BTNS."/user.png"); ?>");margin: 6px 0 0 1px;}
 
 </style>
